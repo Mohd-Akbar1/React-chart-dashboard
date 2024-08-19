6aa7fc38dfbfaf277cf40bbbeb11cd467ec88912
Mohd-Akbar1 <34mohdakbar@gmail.com>
2024-08-19 16:50:19 +0530

    database updating

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const express=require('express')
 const app=express()
 const fs=require('fs')
@@ -5,8 +7,10 @@ const mongoose=require('mongoose')
 const Data = require('./Database/model')
 const cors=require('cors')
 
-mongoose.connect('mongodb://127.0.0.1:27017/BlackCoffer').then(()=>console.log('DB connected sucessfully')).catch(err => console.error('MongoDB connection error:', err));
-
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
 app.use(cors())
 
 app.get('/apidata', async (req, res) => {
